66b801e702de1fe27dd04ca189a94a6fa3bf23db
Your Name <your_email@example.com>
2025-12-31 18:22:39 +0500

    Add indexx.html and indexx.js

---
+++ indexx.js
@@ -0,0 +1,35 @@
+////acceptinng input  from user
+//let name=prompt("enter your name");
+//console.log("hello"+ name +"!")
+
+////displa age using
+//let age=prompt("enter your age")
+//console.log("ypur age is"+ age)
+
+////rounding a decimal
+//let value=7.6 
+//console.log("Rounded value=", Math.round(value));
+
+////rounding a decimal
+//let value=7.6
+//console.log("rounded value=", Math.ceil(value));
+
+////rounding a decimal
+//let value=7.6
+//console.log("rounded value=", Math.floor(value));
+
+////random number between  1-10
+//let randomnum=Math.floor(Math.random()*10)+1;
+//console.log("random number:", randomnum);
+
+////power
+//let x = 2;   // base
+//let y = 3;   // exponent
+//console.log(Math.pow(x, y));
+
+//number guessing
+let n = Math.floor(Math.random() * 10) + 1;
+let g = Number(prompt("Guess 1-10:"));
+console.log(g === n ? "You guessed it!" : "Wrong, it was " + n);
+
+
